3ae93b7d62f20843a5b0fc8d5aef229276fde7ef
Sunny <hhe330@naver.com>
2020-09-11 11:17:13 +0900

    시간, 출처 표기 (moment 활용)

--- Components/DataItem.js
+++ Components/DataItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { ListItem, Thumbnail, Text, Left, Body, Right, Button } from 'native-base';
+import { ListItem, Thumbnail, View, Text, Left, Body, Right, Button } from 'native-base';
+import moment from 'moment';
 
 function DataItem({ article }) {
   return (
@@ -10,6 +11,16 @@ function DataItem({ article }) {
       <Body>
         <Text numberOfLines={2}>{article.title}</Text>
         <Text note numberOfLines={2}>{article.description}</Text>
+
+        {/* 출처, 시간 표시 */}
+        <Text numberOfLines={2}>{article.title}</Text>
+        <Text note numberOfLines={2}>{article.description}</Text>
+        <View style={{ flex: 1, flexDirection: 'row', marginTop: 10 }}>
+          {/* 출처 */}
+          <Text note>{article.source.name}  |  </Text>
+          {/* 시간 */}
+          <Text note>{moment(article.publishedAt || moment.now).fromNow()}</Text>
+        </View>
       </Body>
       <Right>
         <Button transparent>
