02a989a23faf7198b8b101c422c9ea1ef8e8a0aa
Sunny <hhe330@naver.com>
2020-09-11 14:08:11 +0900

    Modal 파일 생성 (Webview 보여줌)

---
+++ Components/ModalView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Modal, Share, Text, View } from 'react-native';
+
+export default function ModalView() {
+  return (
+    <Modal>
+      <View>
+        <Text>모달 페이지</Text>
+      </View>
+    </Modal>
+  );
+};
